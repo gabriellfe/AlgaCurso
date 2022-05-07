f88e4d98b75114316b143ee3a60552d0fbbdab76
gabriellfe <gabriell.marquesfe@hotmail.com>
2022-05-07 15:23:42 -0300

    Melhorando

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/dissembler/RestauranteDissembler.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.api.dissembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+
+@Component
+public class RestauranteDissembler {
+	
+	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
+		Restaurante restaurante = new Restaurante();
+		restaurante.setNome(restauranteInput.getNome());
+		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
+
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(restauranteInput.getCozinha().getId());
+
+		restaurante.setCozinha(cozinha);
+
+		return restaurante;
+	}
+	
+	public List<Restaurante> toCollectionDomain(List<RestauranteInput> restaurantes) {
+		return restaurantes.stream()
+				.map(restaurante -> toDomainObject(restaurante))
+				.collect(Collectors.toList());
+	}
+	
+
+}
