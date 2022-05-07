d374396a3d3543ddbd796be1d82bc061098f51fe
gabriellfe <gabriell.marquesfe@hotmail.com>
2022-05-07 16:12:46 -0300

    Melhorando com ModelMapper

--- algafood-api/src/main/java/com/algaworks/algafood/api/disassembler/RestauranteDisassembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/disassembler/RestauranteDisassembler.java
@@ -3,26 +3,22 @@ package com.algaworks.algafood.api.disassembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.RestauranteInput;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDisassembler {
 	
+	@Autowired
+	private ModelMapper modelMapper;
+
+	
 	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-
-		restaurante.setCozinha(cozinha);
-
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 	
 	public List<Restaurante> toCollectionDomain(List<RestauranteInput> restaurantes) {
